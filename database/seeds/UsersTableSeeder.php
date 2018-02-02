6aa7be01b42257f23f0485f2e995074f340d0fbe
Keesha Cain <keeshacain@Keeshas-MacBook-Pro.local>
2018-02-01 21:30:10 -0700

    create controllers, models, factories, dummmy data round one

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table('users')->insert([
+        //   'username' => str_random(10),
+        //   'email' => str_random(10).'@gmail.com',
+        //   'password' => bcrypt('password'),
+        //   'name' => str_random(10),
+        //   'token' => bcrypt('token')
+        // ]);
+        $users = factory(App\User::class, 100)->create();
+    }
+}
