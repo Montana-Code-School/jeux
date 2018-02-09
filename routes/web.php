3d25ca2f369c49d478ca96030c4f6c67ad8adb2e
Matt Sutton <mcsutton@outlook.com>
2018-02-09 15:55:05 -0700

    created user route, page, and profile component

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/user', function () {
+    return view('user');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
