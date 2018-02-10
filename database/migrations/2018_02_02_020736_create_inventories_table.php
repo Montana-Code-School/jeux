940401b4d62c7d68c5767962b02500df65c099b1
Kelsey Iverson <Kelseymiverson@gmail.com>
2018-02-10 13:59:54 -0700

    multiple inventories seeded

--- database/migrations/2018_02_02_020736_create_inventories_table.php
+++ database/migrations/2018_02_02_020736_create_inventories_table.php
@@ -17,11 +17,10 @@ class CreateInventoriesTable extends Migration
             $table->increments('id');
             $table->integer('game_id')->unsigned();
             //$table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
-            $table->integer('borrower_id')->unsigned();
+            $table->integer('borrower_id')->unsigned()->nullable();
             $table->integer('owner_id')->unsigned();
-            $table->boolean('borrowed');
-            $table->date('date_borrowed');
-            $table->date('date_returned');
+            $table->date('date_borrowed')->nullable();
+            $table->date('date_returned')->nullable();
         });
     }
 
