6de16701b66b89ef1004f98e72d32c814ae01fe2
Keesha Cain <keeshacain@Keeshas-MacBook-Pro.local>
2018-03-03 14:58:55 -0700

    4 users, all have games, all have inventories

--- database/factories/GameFactory.php
+++ database/factories/GameFactory.php
@@ -11,7 +11,13 @@ $factory->define(App\Game::class, function (Faker $faker) {
         'min_age' => rand(5,99),
       //  'min_play' => rand(30, 60),
       //  'max_play' => rand(60,120),
-        'description' => $faker->realText(200),
+        'description' =>
+          'To begin the game you have selected, hit the BOP IT button '
+          . 'on either side of the game unit. Select a game by pulling '
+          . 'the PULL IT knob repeatedly until you hear the name of the '
+          . 'game you want to play: SOLO, PASS IT, or PARTY. BOP IT will '
+          . 'command you to do these 4 actions in random order! Respond '
+          . 'as quickly as you can.',
         'instructions' => $faker->boolean
     ];
 });
