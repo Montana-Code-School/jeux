646bc0ae92b85966e1a5f4a86aed8e40a59a969e
Matt Sutton <mcsutton@outlook.com>
2018-03-10 13:45:45 -0700

    worked on auth user, user profile, and game inventory

--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -1,9 +1,9 @@
 
 <div class="user-profile-container">
   <div class="user-profile">
-    <img src="images/avatar.png" alt="example Avatar" />
+    <img src="{{ asset( "images/" . $user['image'] ) }}" alt="example Avatar" />
     <div class="user-info">
-      <h4>Username HERE</h4>
+      <h4>{{ $user['username'] }}</h4>
       <span>
         Borrowing: 2 Games
       </span>
