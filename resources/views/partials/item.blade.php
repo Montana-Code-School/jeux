0ffec5e834fd567355c72c7908ece211dfad969c
Matt Sutton <mcsutton@outlook.com>
2018-03-10 16:17:59 -0700

    fixed lightbox issue

--- resources/views/partials/item.blade.php
+++ resources/views/partials/item.blade.php
@@ -4,12 +4,27 @@
       <img src="{{ $game['image'] }}" alt="{{ $game['name'] }}"/>
     </div>
     <div class="tile-back back">
-      <a href="#" data-featherlight="#mylightbox">
+      <a href="#" data-featherlight="#lb{{ $index }}">
         <h4>{{ $game['name'] }}</h4>
         <p>Age: {{ $game['min_age'] }}</p>
-        <p>Time: 30-45 mins</p>
-        <p>Players: 2</p>
+        <p>Time: {{$game['min_play']}}-{{$game['max_play']}} mins</p>
+        <p>Players: {{$game['min_player']}}</p>
       </a>
+    </div>
+  </div>
+  <div id="lb{{ $index }}" class="item-full">
+    <div class="item-content">
+      <img src="{{ asset('images/gameAvatar.jpg')}}" alt="game"></img>
+      <div class="item-info">
+        <h4>description</h4>
+        <p>This is a description of the game blah lahbah. You will lose because pandemic hates you. This is a description of the game blah lahbah. You will lose because pandemic hates you. This is a description of the game blah lahbah. You will lose because pandemic hates you. </p>
+        <h5>Players</h5>
+        <p>12</p>
+        <h5>Age</h5>
+        <p>12+</p>
+        <h5>Play Time</h5>
+        <p>15-30mins</p>
+      </div>
     </div>
   </div>
 </div>
