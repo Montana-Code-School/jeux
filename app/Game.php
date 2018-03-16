3882eb56ffceb2a00948ba782686dc959fd426bb
Matt Sutton <mcsutton@outlook.com>
2018-03-16 16:47:39 -0600

    formatting user page code

--- app/Game.php
+++ app/Game.php
@@ -23,60 +23,60 @@ class Game extends Model
     }
 
     public function gameInfo($game_ids) {
-      $games =[];
+        $games =[];
         // setup auth user friend and game ids
         $auth_user = Auth::user()->with('friends', 'inventory')->first();
         $auth_game_ids = [];
         $auth_friend_ids = [];
 
         foreach ($auth_user->friends as $friend) {
-          array_push($auth_friend_ids, $friend->id);
+            array_push($auth_friend_ids, $friend->id);
         }
 
         foreach ($auth_user->inventory as $game) {
-          array_push($auth_game_ids, $game->game_id);
+            array_push($auth_game_ids, $game->game_id);
         }
 
         // loop through all games provided
         foreach ($game_ids as $game_id) {
-          $gameQuery = Game::with('inventory')->where('id', $game_id)->first();
+            $gameQuery = Game::with('inventory')->where('id', $game_id)->first();
 
-          $own_game = (array_search($game_id, $auth_game_ids) !== false) ? true: false;
+            $own_game = (array_search($game_id, $auth_game_ids) !== false) ? true: false;
 
-          $owners = [];
+            $owners = [];
 
-          foreach($gameQuery->inventory as $owner) {
-            if(array_search($owner->owner_id, $auth_friend_ids)) {
-              $user = User::find($owner->owner_id);
+            foreach($gameQuery->inventory as $owner) {
+                if(array_search($owner->owner_id, $auth_friend_ids)) {
+                    $user = User::find($owner->owner_id);
 
-              $user =[
-                'id'=>$user->id,
-                'username'=>$user->username,
-                'image'=>$user->image,
-              ];
+                    $user =[
+                        'id'=>$user->id,
+                        'username'=>$user->username,
+                        'image'=>$user->image,
+                    ];
 
-              // Add user to the owner array
-              array_push($owners, $user);
+                    // Add user to the owner array
+                    array_push($owners, $user);
+                }
             }
-          }
 
-          $game = [
-            'game_id'=>$gameQuery->id,
-            'own_game'=>$own_game,
-            'name'=>$gameQuery->name,
-            'image'=>$gameQuery->image,
-            'year'=>$gameQuery->year,
-            'min_player'=>$gameQuery->min_player,
-            'max_player'=>$gameQuery->max_player,
-            'min_age'=>$gameQuery->min_age,
-            'min_play'=>$gameQuery->min_play,
-            'max_play'=>$gameQuery->max_play,
-            'description'=>$gameQuery->description,
-            'instructions'=>$gameQuery->instructions,
-            'owner'=>$owners,
-          ];
+            $game = [
+                'game_id'=>$gameQuery->id,
+                'own_game'=>$own_game,
+                'name'=>$gameQuery->name,
+                'image'=>$gameQuery->image,
+                'year'=>$gameQuery->year,
+                'min_player'=>$gameQuery->min_player,
+                'max_player'=>$gameQuery->max_player,
+                'min_age'=>$gameQuery->min_age,
+                'min_play'=>$gameQuery->min_play,
+                'max_play'=>$gameQuery->max_play,
+                'description'=>$gameQuery->description,
+                'instructions'=>$gameQuery->instructions,
+                'owner'=>$owners,
+            ];
 
-          array_push($games, $game);
+            array_push($games, $game);
         }
 
         return $games;
