05c8f2563dd3f6e277798d4b79d0cd46c0e38e9e
Cassidy <macair@Cassidys-MacBook-Air.local>
2018-03-20 22:39:32 -0600

    WIP:login page styling

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,21 +1,20 @@
 @extends('layouts.landing')
 @section('content')
 
- <div id="title-sketch-holder">
- </div>
+<div id="title-sketch-holder">
+</div>
 
- <div class="title-container">
-   <h1>b<sup>3</sup></h1>
- </div>
+<div class="title-container"</a>>
+  <h1><a href="{{ url('/login') }}">b<sup>3</sup></a></h1>
+</div>
 
- <script>
- setInterval(function(){
-   var hue = ((Math.sin(performance.now()/500)+1)/2) * 360;
-   var value = "hsl("+hue+", 100%, 50%)"
-   $(".title-container h1").css("color", value);
- }, 30);
-
- </script>
+<script>
+setInterval(function(){
+  var hue = ((Math.sin(performance.now()/500)+1)/2) * 360;
+  var value = "hsl("+hue+", 100%, 50%)"
+  $(".title-container h1").css("color", value);
+}, 30);
+</script>
 
 <script type='text/javascript' src="{{ asset('js/title-sketch.js') }}"></script>
 @endsection
