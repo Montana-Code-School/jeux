533380dac01e9359bfed8582c844a03f56b05f0b
Kerry <zahnahoria@yahoo.com>
2018-03-28 21:33:35 -0600

    taking out an ending button tag

--- resources/views/partials/dropdown-item.blade.php
+++ resources/views/partials/dropdown-item.blade.php
@@ -17,8 +17,7 @@
       <a class="btn btn-default" href='{{$action['url']}}'>
         <span class="glyphicon {{$action['glyphicon']}}"></span>
       </a>
-
-      </button>
+      
     @endforeach
   @endif
 </div>
