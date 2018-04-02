7d610c69fc9020fa530945f9f4b42b9b4ca3c113
Kelsey Iverson <Kelseymiverson@gmail.com>
2018-04-02 04:47:05 -0600

    made all element tests for search result page, as well as changed the login test to search for the, then log out

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\Browser\Pages\Landing;
 use Tests\Browser\Pages\Login;
 use Tests\Browser\Pages\Home;
+use Tests\Browser\Pages\SearchResults;
 
 class LoginTest extends DuskTestCase
 {
@@ -31,12 +32,18 @@ class LoginTest extends DuskTestCase
                     ->on(new Home)
                     ->pause(2000)
                     ->mouseover('@tile-game-image')
-                    ->type('@search-games-input', 'settlers')
+                    ->type('@search-games-input', 'the')
                     ->pause(3000)
                     ->click('@search-games-submit')
                     ->pause(3000)
                     ->mouseover('@tile-game-image')
-                    ->pause(2000);
+                    ->pause(2000)
+                    ->on(new SearchResults)
+                    ->pause(2000)
+                    ->click('@user-dropdown-toggle')
+                    ->click('@user-logout')
+                    ->pause(2000)
+                    ->assertUrlIs('http://localhost:8888/');
         });
     }
 }
