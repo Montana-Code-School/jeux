3d50bbf11443596e50f08f29baad208571e4d155
Kelsey Iverson <Kelseymiverson@gmail.com>
2018-04-02 04:10:09 -0600

    all elements are being asserted on Profile page and passing with php artisan dusk

---
+++ tests/Browser/Pages/Profile.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Browser\Pages;
+
+use Laravel\Dusk\Browser;
+use Laravel\Dusk\Page as BasePage;
+use Tests\Browser\Components\Navbar;
+
+class Profile extends BasePage
+{
+    /**
+     * Get the URL for the page.
+     *
+     * @return string
+     */
+    public function url()
+    {
+        return '/users/Frankie1';
+    }
+
+    /**
+     * Assert that the browser is on the page.
+     *
+     * @param  Browser  $browser
+     * @return void
+     */
+    public function assert(Browser $browser)
+    {
+        $browser->assertPathIs($this->url())
+        //testing the navBar
+                ->assertSee('My Games')
+                ->assertSee('Browse')
+                ->assertInputValue('@search-games-input', '')
+                ->assertVisible('@search-games-submit')
+                ->assertVisible('@friends-dropdown-list')
+                ->assertVisible('@dropdown-notify-content')
+                ->assertVisible('@friends-dropdown-toggle')
+                ->assertVisible('@dropdown-notify-toggle')
+                ->assertVisible('@user-dropdown-toggle')
+                ->click('@user-dropdown-toggle')
+                ->assertVisible('@user-profile')
+                ->assertVisible('@user-settings')
+                ->assertVisible('@user-logout')
+        //testing the elements on Home
+                ->assertSee('User Profile')
+                ->assertVisible('.user-profile')
+                ->assertVisible('.user-info')
+                ->assertVisible('@tile-game-image')
+        //testing the elements on Filter
+                ->assertSee('Options')
+                ->assertVisible('@filter')
+                ->assertVisible('@age-slider')
+                ->assertVisible('@players-slider')
+                ->assertVisible('@Play-time-drop-down')
+                ->assertVisible('@genre-button')
+                ->assertVisible('@add-game-button')
+                ->assertVisible('@filter-submit');
+    }
+
+    /**
+     * Get the element shortcuts for the page.
+     *
+     * @return array
+     */
+    public function elements()
+    {
+        return [
+            '@element' => '#selector',
+        ];
+    }
+}
